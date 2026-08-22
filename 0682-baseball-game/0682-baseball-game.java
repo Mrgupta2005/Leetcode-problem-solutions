9e8cb0388c35493ac001430aecff2990f170c725
Harshit Gupta <125191441+Mrgupta2005@users.noreply.github.com>
2026-08-22 10:01:01 +0530

    Time: 3 ms (80.66%), Space: 43.2 MB (92.27%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int calPoints(String[] operations) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (String op : operations) {
+
+            if (op.equals("C")) {
+                stack.pop();
+            }
+            else if (op.equals("D")) {
+                stack.push(2 * stack.peek());
+            }
+            else if (op.equals("+")) {
+                int last = stack.pop();
+                int second = stack.peek();
+
+                stack.push(last);
+                stack.push(last + second);
+            }
+            else {
+                stack.push(Integer.parseInt(op));
+            }
+        }
+
+        int sum = 0;
+
+        for (int score : stack) {
+            sum += score;
+        }
+
+        return sum;
+    }
+}
